7d39e438164d9ef62a3a7cf89004095412a68b93
Weston Ellis <ellisweston112@gmail.com>
2021-11-13 09:35:16 -0700

    Add open weather service

---
+++ app/services/open_weather_service.rb
@@ -0,0 +1,13 @@
+class OpenWeatherService
+
+  def self.forcast(lat, long)
+    response = Faraday.get('https://api.openweathermap.org/data/2.5/onecall') do |f|
+      f.params['appid'] = ENV['open_weather_key']
+      f.params['lat'] = lat
+      f.params['lon'] = long
+      f.params['units'] = 'imperial'
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
