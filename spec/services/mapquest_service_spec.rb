605e7967756eeee925b67cb2926cf60e391e1df4
Weston Ellis <ellisweston112@gmail.com>
2021-11-12 17:34:16 -0700

    Add location data

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -3,17 +3,17 @@ require "rails_helper"
 
 RSpec.describe MapquestService do
 
-  it 'can retrieve location with city/state' do
+  it 'can retrieve location with city/state', :vcr do
     response = MapquestService.location_info('denver,co')
     body = JSON.parse(response.body, symbolize_names: true)
 
     expect(body).to be_a(Hash)
     expect(body).to have_key(:info)
     expect(body).to have_key(:results)
-    expect(body[:results]).to have_key(:latlng)
-    expect(body[:results][:latlng]).to have_key(:lat)
-    expect(body[:results][:latlng][:lat]).to eq(39.738453)
-    expect(body[:results][:latlng]).to have_key(:lng)
-    expect(body[:results][:latlng][:lng]).to eq(-104.984853)
+    expect(body[:results][0][:locations][0]).to have_key(:latLng)
+    expect(body[:results][0][:locations][0][:latLng]).to have_key(:lat)
+    expect(body[:results][0][:locations][0][:latLng][:lat]).to eq(39.738453)
+    expect(body[:results][0][:locations][0][:latLng]).to have_key(:lng)
+    expect(body[:results][0][:locations][0][:latLng][:lng]).to eq(-104.984853)
   end
 end
