ffe2df14546e269d201485b0e207b3ff27cdcab7
Weston Ellis <ellisweston112@gmail.com>
2021-11-13 10:40:43 -0700

    Write daily weather

---
+++ spec/poros/daily_weather_spec.rb
@@ -0,0 +1,56 @@
+require "rails_helper"
+
+RSpec.describe DailyWeather do
+
+  it 'exists and has attributes' do
+    data = {
+        "dt": 1636826400,
+        "sunrise": 1636810962,
+        "sunset": 1636847163,
+        "moonrise": 1636838880,
+        "moonset": 1636790160,
+        "moon_phase": 0.33,
+        "temp": {
+            "day": 60.84,
+            "min": 46.99,
+            "max": 64.63,
+            "night": 51.31,
+            "eve": 59.11,
+            "morn": 47.57
+        },
+        "feels_like": {
+            "day": 57.85,
+            "night": 47.75,
+            "eve": 55.76,
+            "morn": 44.98
+        },
+        "pressure": 1017,
+        "humidity": 26,
+        "dew_point": 26.49,
+        "wind_speed": 8.21,
+        "wind_deg": 299,
+        "wind_gust": 27.89,
+        "weather": [
+            {
+                "id": 804,
+                "main": "Clouds",
+                "description": "overcast clouds",
+                "icon": "04d"
+            }
+        ],
+        "clouds": 100,
+        "pop": 0,
+        "uvi": 2.39
+    }
+
+    weather = DailyWeather.new(data)
+
+    expect(weather.date).to eq("2021-11-13")
+    expect(weather.sunrise).to eq('2021-11-13 06:42:42 -0700')
+    expect(weather.sunset).to eq('2021-11-13 16:46:03 -0700')
+    expect(weather.max_temp).to eq(64.63)
+    expect(weather.min_temp).to eq(46.99)
+    expect(weather.conditions).to eq("overcast clouds")
+    expect(weather.icon).to eq("04d")
+  end
+end
