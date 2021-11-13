935cd5203e99eff4a2e4212216e61890f96006cf
Weston Ellis <ellisweston112@gmail.com>
2021-11-13 14:18:24 -0700

    Add framework

---
+++ spec/requests/api/v1/background_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe 'background image search endpoint' do
+
+  it 'has the correct structure' do
+    get '/api/v1/backgrounds?location=denver,co'
+
+    expect(response).to be_successful
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body).to have_key(:data)
+    expect(body[:data]).to have_key(:id)
+    expect(body[:data]).to have_key(:attributes)
+    expect(body[:data][:attributes]).to have_key(:location)
+    expect(body[:data][:attributes]).to have_key(:image_url)
+    expect(body[:data][:attributes]).to have_key(:credit)
+    expect(body[:data][:attributes][:credit]).to have_key(:source)
+    expect(body[:data][:attributes][:credit]).to have_key(:photographer)
+    expect(body[:data][:attributes][:credit]).to have_key(:photographer_portfolio)
+  end
+end
