90714e8299882bf99a82affea8e69480a93e0c7c
Weston Ellis <ellisweston112@gmail.com>
2021-11-15 15:35:58 -0700

    Finish endpoint

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,14 +4,14 @@ class Api::V1::SessionsController < ApplicationController
   def create
 
     user = User.find_by(email: params[:email])
-
-    if user && user.validate(params[:password])
+    # require "pry"; binding.pry
+    if user && user.authenticate(params[:password])
       # o:m for future scalability, but this is needed for o:m
       key = user.api_keys.first
       #bad naming convention
       render json: UserSerializer.new_user(user, key)
-    else
-      #403 bby
+    elsif user.nil? || !user.authenticate(params[:password])
+      render json: { body: 'bad credentials' }, status: 403
     end
   end
 
--- spec/requests/api/v1/users/login_user_spec.rb
+++ spec/requests/api/v1/users/login_user_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe 'user login request' do
       email: user.email,
       password: 'password'
     }
+    @bad_credentials = {
+      email: 'user.email@fake.com',
+      password: 'password1'
+    }
   end
 
   it 'successfully logs a user in' do
@@ -25,5 +29,16 @@ RSpec.describe 'user login request' do
     expect(body[:data]).to have_key(:attributes)
     expect(body[:data][:attributes]).to have_key(:email)
     expect(body[:data][:attributes]).to have_key(:api_key)
+  end
+
+  it 'returns 403 with bad credentials' do
+    post '/api/v1/sessions', params: @bad_credentials
+
+
+    expect(response.status).to eq(403)
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body[:body]).to eq('bad credentials')
   end
 end
