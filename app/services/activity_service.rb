fa0106fa522799123e41300552c4b54ab1ebb70b
Weston Ellis <ellisweston112@gmail.com>
2021-11-15 10:00:07 -0700

    Finish service

--- app/services/activity_service.rb
+++ app/services/activity_service.rb
@@ -4,4 +4,19 @@ class ActivityService
     response = Faraday.get('http://www.boredapi.com/api/activity', {type: 'relaxation'})
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.recreational
+    response = Faraday.get('http://www.boredapi.com/api/activity', {type: 'recreational'})
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.busywork
+    response = Faraday.get('http://www.boredapi.com/api/activity', {type: 'busywork'})
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.cooking
+    response = Faraday.get('http://www.boredapi.com/api/activity', {type: 'cooking'})
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/activity_service_spec.rb
+++ spec/services/activity_service_spec.rb
@@ -4,11 +4,43 @@ RSpec.describe ActivityService do
 
   it 'can get relaxation activity', :vcr do
     response = ActivityService.relaxation
-    require "pry"; binding.pry
+
     expect(response).to have_key(:activity)
     expect(response).to have_key(:type)
+    expect(response[:type]).to eq('relaxation')
     expect(response).to have_key(:participants)
     expect(response).to have_key(:price)
   end
 
+  it 'can get recreational activity', :vcr do
+    response = ActivityService.recreational
+
+    expect(response).to have_key(:activity)
+    expect(response).to have_key(:type)
+    expect(response[:type]).to eq('recreational')
+    expect(response).to have_key(:participants)
+    expect(response).to have_key(:price)
+  end
+
+  it 'can get busywork activity', :vcr do
+    response = ActivityService.busywork
+
+
+    expect(response).to have_key(:activity)
+    expect(response).to have_key(:type)
+    expect(response[:type]).to eq('busywork')
+    expect(response).to have_key(:participants)
+    expect(response).to have_key(:price)
+  end
+
+  it 'can get cooking activity', :vcr do
+    response = ActivityService.cooking
+
+
+    expect(response).to have_key(:activity)
+    expect(response).to have_key(:type)
+    expect(response[:type]).to eq('cooking')
+    expect(response).to have_key(:participants)
+    expect(response).to have_key(:price)
+  end
 end
