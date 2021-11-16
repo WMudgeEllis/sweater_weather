cceec1c9c03937b924e2fc923bea82a78285193d
Weston Ellis <ellisweston112@gmail.com>
2021-11-16 16:54:41 -0700

    Update test

--- spec/serializers/open_weather_serializer_spec.rb
+++ spec/serializers/open_weather_serializer_spec.rb
@@ -6,8 +6,16 @@ RSpec.describe OpenWeatherSerializer do
       lat: 39.738453,
       long: -104.984853
     }
-
     forcast = OpenWeatherSerializer.forcast(coords)
-    # TODO: test test test
+
+    expect(forcast[:data]).to be_a(Hash)
+    expect(forcast[:data][:id]).to eq(nil)
+    expect(forcast[:data][:type]).to eq('forecast')
+    expect(forcast[:data][:attributes]).to be_a(Hash)
+    expect(forcast[:data][:attributes][:current_weather]).to be_a(Hash)
+    expect(forcast[:data][:attributes][:daily_weather]).to be_a(Array)
+    expect(forcast[:data][:attributes][:daily_weather].length).to eq(5)
+    expect(forcast[:data][:attributes][:hourly_weather]).to be_a(Array)
+    expect(forcast[:data][:attributes][:hourly_weather].length).to eq(8)
   end
 end
